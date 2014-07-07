89da409a77d700cb64ce6f2f5e4354d6b02bf7ad
sapherise <sa@otherfor.com>
2014-07-07 22:34:24 +0800

    Initial Commit

---
+++ Nian iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nian iOS
+//
+//  Created by Sa on 14-7-7.
+//  Copyright (c) 2014年 Sa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
