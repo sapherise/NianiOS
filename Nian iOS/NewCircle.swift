09c403a24ef88a38219f6fa83000c88ebf83131e
vizee <vizee@live.cn>
2014-12-05 15:19:24 +0800

    update

---
+++ Nian iOS/NewCircle.swift
@@ -0,0 +1,16 @@
+//
+//  NewCircle.swift
+//  Nian iOS
+//
+//  Created by vizee on 14/12/4.
+//  Copyright (c) 2014年 Sa. All rights reserved.
+//
+
+import Foundation
+
+class NewCircleViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
