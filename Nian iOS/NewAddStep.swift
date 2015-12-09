7f5b1e7fb0cf6643325a8a114494144d96e67794
BobWei <fealonelei@gmail.com>
2015-12-09 20:23:32 +0800

    12.09 保存 -- 多图进展 debugging

---
+++ Nian iOS/NewAddStep.swift
@@ -0,0 +1,21 @@
+//
+//  NewAddStep.swift
+//  Nian iOS
+//
+//  Created by WebosterBob on 12/9/15.
+//  Copyright © 2015 Sa. All rights reserved.
+//
+
+import UIKit
+
+class NewAddStep: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
